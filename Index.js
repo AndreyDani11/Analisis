815789a1224a540c30adaa71dd1416e48e825c10
AndreyDani <andrealealnazareth@gmail.com>
2022-02-28 07:35:46 -0500

    solucion taller 1

---
+++ Index.js
@@ -0,0 +1,44 @@
+console.log("Hola Mundo");
+// comentarios en linea
+/*comentario 
+de 
+varias
+lineas
+*/
+
+// Variables
+var primerVariable;
+let edadUsuario;
+//Declarar variables
+var nombreVariable;
+//Crear variable sin asignar valor
+/* 
+Inicializar variable:
+Asignar un valor al momento de crear la variable
+*/
+var gradoEstudiantes="10A";
+/*
+Asignar un valor:
+Igualar una variable existente a un nuevo valor
+*/
+edadUsuario=16;
+//Asignar valor desde otra variable
+console.log(edadUsuario);
+var edad=17;
+edadUsuario=edad;
+console.log(edadUsuario);
+
+
+//por teclado
+
+var apellido=prompt("ingrese su apellido");
+console.log(apellido);
+//Constantes: son valores que no pueden ser cambiados a lo largo del codigo. Siempre deben ser inicializadas. El nombre debe ir en mayuscula
+const PI=3.1416;
+
+//Imprimir por pantalla
+
+console.log(edadUsuario); // imprimir variable
+console.log("hola este es un mensaje"); // imprimir un mensaje
+console.log("El usuario tiene " + edadUsuario + " años"); //concatena mensajes y variables
+
---
+++ taller1.js
@@ -0,0 +1,15 @@
+//Archivo de práctica y pruebas de JavaScript
+/*
+Taller 1
+Fecha 28/02/2022
+Realizado por: Andrea Leal Ospina
+*/
+var nombreEstudiante; //string
+var gradoEstudiante="10B"; //string
+let edadEstudiante; //int
+nombreEstudiante="Andrea Leal Ospina";
+edadEstudiante=prompt("ingrese su edad");
+console.log("El estudiante " + nombreEstudiante + " del grado " + gradoEstudiante + " tiene " + edadEstudiante + " años");
+
+
+
