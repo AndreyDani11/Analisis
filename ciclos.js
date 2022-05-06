619ccb13be314ac8e7dce876955e1f6c2242e8bf
AndreyDani <andrealealnazareth@gmail.com>
2022-05-06 11:45:58 -0500

    evaluacionCondicionales

--- ciclos.js
+++ ciclos.js
@@ -1,4 +1,33 @@
-Ciclo While 
+/*Ciclo While 
 Estructura: While(condicion){
     instrucciones
+}
+*/
+
+/*
+var CantSaludos = 0;
+ 
+while(CantSaludos < 5){
+    console.log("Hola ");
+    CantSaludos += 1;
+}
+
+var respuesta ="si";
+ 
+while(respuesta=="si"){
+    console.log("Dijo sí");
+    respuesta=prompt("ingrese un valor");
+}
+*/
+
+
+var valor = true;
+var respuesta2;
+ 
+while(valor){
+    console.log("valor es " + valor);
+    respuesta2=prompt("ingrese un valor 1 verdadero 0 falso");
+    if(respuesta2 == 0){
+        valor = false;
+    }      
 }
